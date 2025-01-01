4b7fbdaa001324be5455034bceb7d6eb9ad0b6b8
arungeorgesaji <arungeorgesaji@gmail.com>
2025-01-01 22:05:03 +0530

    Switch the distance measurement to use depth anything

--- object_detection.py
+++ object_detection.py
@@ -1,27 +1,22 @@
+from transformers import pipeline
 import os
-from pathlib import Path
 from ultralytics import YOLO
+from gtts import gTTS
 import math
 import yaml
 import cv2
+from PIL import Image
+import numpy as np
 
-sensor_width = 790
-focal_length = 3.63
+image_filename = "captured_image.jpg"
+model = YOLO('models/yolov8m-seg.pt')
+pipe = pipeline(task="depth-estimation", model="depth-anything/Depth-Anything-V2-Small-hf")
 
-MODEL_DIR = Path("models")
-MODEL_DIR.mkdir(exist_ok=True) 
-model = YOLO(MODEL_DIR / "yolov8m-seg.pt")
-
-def calculate_distance(sensor_width, focal_length, object_pixel_width, screen_pixel_width, true_width):
-    fov = 2 * math.atan(0.5 * sensor_width / focal_length)
-    angle_of_arc = fov * (object_pixel_width / screen_pixel_width)
-    true_distance = (true_width / 2) / math.tan(angle_of_arc / 2)
-    return true_distance / 240
-
-def load_object_widths(filename):
-    with open(filename, 'r') as file:
-        data = yaml.safe_load(file)
-    return data
+def convert_opencv_to_pil(opencv_image):
+    rgb_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
+    pil_image = Image.fromarray(rgb_image)
+    
+    return pil_image
 
 def check_location(x_min, y_min, x_max, y_max, frame_width, frame_height):
     object_center_x = (x_min + x_max) // 2
@@ -39,30 +34,37 @@ def check_location(x_min, y_min, x_max, y_max, frame_width, frame_height):
     else:
         return "front"
 
-def detect_objects(image_filename):
-    frame = cv2.imread(image_filename)
+def scan_mode():
+    try:
+        cap = cv2.VideoCapture(1)
+        ret, frame = cap.read()
+        cv2.imwrite(image_filename, frame)
+        frame = cv2.imread(image_filename)
 
-    if frame is None:
-        print("Error: Unable to load image.")
-        return
+        if frame is None:
+            print("Error: Unable to load image.")
+            return
 
-    results = model(frame)
-    names = model.names
-    frame_height, frame_width, _ = frame.shape
+        results = model(frame)
+        names = model.names
+        frame_height, frame_width, _ = frame.shape
     
-    for result in results:
-        for box in result.boxes:
-            coordinates = box.xyxy
-            name = names[int(box.cls)]
-            x_min, y_min, x_max, y_max = coordinates[0][0].item(),coordinates[0][1].item(),coordinates[0][2].item(),coordinates[0][3].item()
-            position = check_location(int(x_min), int(y_min), int(x_max), int(y_max), frame_width, frame_height)
-            if name in object_widths:
-                true_width = object_widths[name]  
-                distance = calculate_distance(sensor_width, focal_length, int(x_max)-int(x_min), frame_width, true_width)
-                message = f"There is a {name}, around {distance:.2f} centimeters away and it is in your {position}."
-                return message
-            else:
-                return "no_object"
-
-
-object_widths = load_object_widths("object_widths.yaml")
+        for result in results:
+            for box in result.boxes:
+                coordinates = box.xyxy
+                name = names[int(box.cls)]
+                x_min, y_min, x_max, y_max = coordinates[0][0].item(),coordinates[0][1].item(),coordinates[0][2].item(),coordinates[0][3].item()
+                position = check_location(int(x_min), int(y_min), int(x_max), int(y_max), frame_width, frame_height)
+                image = convert_opencv_to_pil(frame[int(y_min):int(y_max), int(x_min):int(x_max)])
+                depth_map = pipe(image)["depth"]
+                depth_array = np.array(depth_map)  
+                mean_depth = np.median(depth_array)
+                message = f"There is a {name}, around {mean_depth:.2f} centimeters away and it is in your {position}."
+                speak(message)
+    except Exception as e:
+        print(f"Error: {e}")
+        import traceback
+        traceback.print_exc()
+    finally:
+        cap.release()
+        cv2.destroyAllWindows()
