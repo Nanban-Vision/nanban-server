84d84a9c62f6ffec4bfa56287e0cc140f52fbd68
arungeorgesaji <arungeorgesaji@gmail.com>
2025-01-07 18:55:17 +0530

    Added wireguard command also just incase

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #Create Tunnel
 ssh -R nanban.serveo.net:80:localhost:8000 serveo.net
 
+#Wireguard 
+wg-quick up nanban
+
 #Run FastAPI
 python -m uvicorn main:app --host 0.0.0.0 --port 8000 --reload
