932d70e956a615fc9ae6ff103757ab4639510e47
arungeorgesaji <arungeorgesaji@gmail.com>
2025-01-27 09:41:51 +0530

    Removed pytube from requirements.txt

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-#Create Tunnel
+#Create Tunnel using ngrok
 ngrok http --url=admittedly-wired-halibut.ngrok-free.app 8000
 
-#Run FastAPI
+#Run FastAPI server
 python -m uvicorn main:app --host 0.0.0.0 --port 8000 --reload
